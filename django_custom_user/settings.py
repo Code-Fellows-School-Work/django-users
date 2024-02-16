9746d176df87ed5c3c70db360abdb2f31636c1f2
evidad <errolvidad@gmail.com>
2024-02-15 17:47:53 -0800

    updated project settings file

--- django_custom_user/settings.py
+++ django_custom_user/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# need to add this when custom user is created
+AUTH_USER_MODEL = 'accounts.CustomUser'
